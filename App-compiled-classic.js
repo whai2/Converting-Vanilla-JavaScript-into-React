10f6fe45215346c5ebaa56763ae30471ef862245
whai2 <whai2125@gmail.com>
2024-07-23 19:33:53 +0900

    feat: 컴파일 폴더로 컴파일 위치 지정, public 폴더를 통해, 최초 html 파일 구성

--- App-compiled-classic.js
+++
@@ -1,3 +0,0 @@
-const App = () => {
-  return /*#__PURE__*/React.createElement("div", null, "Hello, JSX!");
-};
--- App-compiled.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const App = () => {
-  return /*#__PURE__*/_jsx("div", {
-    children: "Hello, JSX!"
-  });
-};
