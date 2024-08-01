463230b272ebcb8df872b8e4192dbacb90f074e5
whai2 <whai2125@gmail.com>
2024-08-01 14:28:45 +0900

    feat: render 함수 추가

--- react/index.js
+++ react/index.js
@@ -1,5 +1,6 @@
 const React = {
   createElement,
+  render
 }
 
 // 1. createElement
@@ -23,6 +24,27 @@ function createTextElement(text) {
       children: [],
     },
   };
+}
+
+// 2. render
+function render(element, container) {
+  // element가 TEXT_ELEMENT인 경우 텍스트 노드를 생성하고, 그렇지 않은 경우 element.type에 해당하는 HTML 요소를 생성
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = key => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name];
+    });
+
+  // 자식 요소들을 재귀적으로 render 함수를 호출하여 DOM 요소에 추가
+  element.props.children.forEach(child => render(child, dom));
+
+  container.appendChild(dom);
 }
 
 export default React;
